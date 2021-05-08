c68f53ff969d4433d31420c34e8fa2c743f387bc
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-05-08 17:03:26 -0500

    Downloading project dependencies

---
+++ app/src/main/java/com/coopera/stream_chat_real_time_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.coopera.stream_chat_real_time_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
