72d4abd9b85c539d9983d0aa4e7dece7be231dc2
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-05-09 17:25:46 -0500

    Validating user login session text inputs

--- app/src/main/java/com/coopera/stream_chat_real_time_demo/ui/login/LoginFragment.kt
+++ app/src/main/java/com/coopera/stream_chat_real_time_demo/ui/login/LoginFragment.kt
@@ -5,56 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.coopera.stream_chat_real_time_demo.R
+import com.coopera.stream_chat_real_time_demo.databinding.FragmentLoginBinding
+import com.coopera.stream_chat_real_time_demo.model.ChatUser
+import com.google.android.material.textfield.TextInputLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.button.setOnClickListener {
+            authenticateTheUser()
+        }
+
+        return binding.root;
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun authenticateTheUser() {
+        val firstName = binding.firstNameEditText.text.toString()
+        val username = binding.usernameEditText.text.toString()
+        if (validateInput(firstName, binding.firstNameInputLayout) &&
+            validateInput(username, binding.usernameInputLayout)
+        ) {
+            val chatUser = ChatUser(firstName, username)
+            val action = LoginFragmentDirections.actionLoginFragmentToChannelFragment(chatUser)
+            findNavController().navigate(action)
+        }
     }
+
+    private fun validateInput(inputText: String, textInputLayout: TextInputLayout): Boolean {
+        return if (inputText.length <= 3) {
+            textInputLayout.isErrorEnabled = true
+            textInputLayout.error = "* Minimum 4 Characters Allowed"
+            false
+        } else {
+            textInputLayout.isErrorEnabled = false
+            textInputLayout.error = null
+            true
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
