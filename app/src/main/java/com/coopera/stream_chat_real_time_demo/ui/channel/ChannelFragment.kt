58f1f18f182b96f726094995d00136e75e504e6d
RobertoMdz <roberto.cr.mendoza@gmail.com>
2021-05-09 18:24:05 -0500

    setUp customer to stream chat session

--- app/src/main/java/com/coopera/stream_chat_real_time_demo/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/coopera/stream_chat_real_time_demo/ui/channel/ChannelFragment.kt
@@ -1,60 +1,177 @@
 package com.coopera.stream_chat_real_time_demo.ui.channel
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.coopera.stream_chat_real_time_demo.R
+import com.coopera.stream_chat_real_time_demo.databinding.FragmentChannelBinding
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Filters
+import io.getstream.chat.android.client.models.User
+import io.getstream.chat.android.client.models.name
+import io.getstream.chat.android.livedata.ChatDomain
+import io.getstream.chat.android.ui.avatar.AvatarView
+import io.getstream.chat.android.ui.channel.list.header.viewmodel.ChannelListHeaderViewModel
+import io.getstream.chat.android.ui.channel.list.header.viewmodel.bindView
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
+import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
+import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChannelFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChannelFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val args: ChannelFragmentArgs by navArgs()
+
+    private var _binding: FragmentChannelBinding? = null
+    private val binding get() = _binding!!
+
+    private val client = ChatClient.instance()
+    private lateinit var user: User
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_channel, container, false)
+        _binding = FragmentChannelBinding.inflate(inflater, container, false)
+
+        setupUser()
+        setupChannels()
+        setupDrawer()
+
+        binding.channelsView.setChannelDeleteClickListener { channel ->
+            deleteChannel(channel)
+        }
+
+        binding.channelListHeaderView.setOnActionButtonClickListener {
+            findNavController().navigate(R.id.action_channelFragment_to_usersFragment)
+        }
+
+        binding.channelsView.setChannelItemClickListener { channel ->
+            val action = ChannelFragmentDirections.actionChannelFragmentToChatFragment(channel.cid)
+            findNavController().navigate(action)
+        }
+
+        binding.channelListHeaderView.setOnUserAvatarClickListener {
+            binding.drawerLayout.openDrawer(Gravity.START)
+        }
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChannelFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChannelFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun setupUser() {
+        if (client.getCurrentUser() == null) {
+            user = if (args.chatUser.firstName.contains("Roberto")) {
+                User(
+                    id = args.chatUser.userName,
+                    extraData = mutableMapOf(
+                        "name" to args.chatUser.firstName,
+                        "country" to "México",
+                        "image" to "https://i.postimg.cc/V6L7rWqC/Dash.png"
+                    )
+                )
+            } else {
+                User(
+                    id = args.chatUser.userName,
+                    extraData = mutableMapOf(
+                        "name" to args.chatUser.firstName
+                    )
+                )
+            }
+            val token = client.devToken(user.id)
+            client.connectUser(
+                user = user,
+                token = token
+            ).enqueue { result ->
+                if (result.isSuccess) {
+                    Log.d("ChannelFragment", "Usuario conectado exitosamente")
+                } else {
+                    Log.d("ChannelFragment", result.error().message.toString())
                 }
             }
+        }
     }
+
+    private fun setupChannels() {
+        val filters = Filters.and(
+            Filters.eq("type", "messaging"),
+            Filters.`in`("members", listOf(client.getCurrentUser()!!.id))
+        )
+        val viewModelFactory = ChannelListViewModelFactory(
+            filters,
+            ChannelListViewModel.DEFAULT_SORT
+        )
+        val listViewModel: ChannelListViewModel by viewModels { viewModelFactory }
+        val listHeaderViewModel: ChannelListHeaderViewModel by viewModels()
+
+        listHeaderViewModel.bindView(binding.channelListHeaderView, viewLifecycleOwner)
+        listViewModel.bindView(binding.channelsView, viewLifecycleOwner)
+    }
+
+    private fun setupDrawer() {
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            if (menuItem.itemId == R.id.logout_menu) {
+                logout()
+            }
+            false
+        }
+        val currentUser = client.getCurrentUser()!!
+        val headerView = binding.navigationView.getHeaderView(0)
+        val headerAvatar = headerView.findViewById<AvatarView>(R.id.avatarView)
+        headerAvatar.setUserData(currentUser)
+        val headerId = headerView.findViewById<TextView>(R.id.id_textView)
+        headerId.text = currentUser.id
+        val headerName = headerView.findViewById<TextView>(R.id.name_textView)
+        headerName.text = currentUser.name
+    }
+
+    private fun logout() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+            client.disconnect()
+            findNavController().navigate(R.id.action_channelFragment_to_loginFragment)
+            showToast("Logged out successfully")
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Logout?")
+        builder.setMessage("Are you sure you want to logout?")
+        builder.create().show()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(
+            requireContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun deleteChannel(channel: Channel) {
+        ChatDomain.instance().useCases.deleteChannel(channel.cid).enqueue { result ->
+            if (result.isSuccess) {
+                showToast("Channel: ${channel.name} removed!")
+            } else {
+                Log.e("ChannelFragment", result.error().message.toString())
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
 }
